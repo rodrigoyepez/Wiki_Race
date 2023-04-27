89f7697617f008ee3ef9af0fc4c0503820d1b16b
rodrigoyepez <76505136+rodrigoyepez@users.noreply.github.com>
2023-04-27 14:03:00 -0400

    New

--- LinksTest.java
+++ LinksTest.java
@@ -1,2 +1,30 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
 public class LinksTest {
+    @Test
+    public void testJsoupGetLinks() {
+        Links links = new Links("Java");
+        ArrayList<String> actualLinks = links.JsoupGetLinks("Java");
+
+        // Perform assertions to validate the expected behavior
+        assertNotNull(actualLinks);
+        assertFalse(actualLinks.isEmpty());
+        assertEquals(20, actualLinks.size());
+        // ... add more assertions based on your expected results
+    }
+
+    @Test
+    public void testRemoveUnwantedStrings() {
+        Links links = new Links("Java");
+        ArrayList<String> inputList = links.JsoupGetLinks("Java");
+        ArrayList<String> actualFiltered = links.removeUnwantedStrings(inputList);
+
+        // Perform assertions to validate the expected behavior
+        assertNotNull(actualFiltered);
+        assertFalse(actualFiltered.isEmpty());
+        assertEquals(4, actualFiltered.size());
+        // ... add more assertions based on your expected results
+    }
 }
--- WikiWordTest.java
+++ WikiWordTest.java
@@ -1,2 +1,18 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
 public class WikiWordTest {
+
+    @Test
+    public void testWikiWrord_generator() {
+        WikiWord word = new WikiWord();
+        String actualFiltered = word.wikiWrord_generator();
+
+        // Perform assertions to validate the expected behavior
+        assertNotNull(actualFiltered);
+        assertFalse(actualFiltered.isEmpty());
+        assertEquals(4, actualFiltered.length());
+        // ... add more assertions based on your expected results
+    }
 }
